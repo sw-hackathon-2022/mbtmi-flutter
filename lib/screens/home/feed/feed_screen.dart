cd2874ed072acbe0aba0e1c6f7eb96c7908a82ed
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-06-23 00:18:23 +0900

    feat : BottomBar 추가

---
+++ lib/screens/home/feed/feed_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: const Text('feed_screen'),
+    );
+  }
+}
